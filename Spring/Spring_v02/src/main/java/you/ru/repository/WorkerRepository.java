9f65d5bf4ac36be39753cfe67798329f0ec699ff
vmokook <147443354+vmokook@users.noreply.github.com>
2024-01-14 23:11:24 +0400

    Delete Spring/Spring_v02 directory

--- Spring/Spring_v02/src/main/java/you/ru/repository/WorkerRepository.java
+++
@@ -1,7 +0,0 @@
-package you.ru.repository;
-
-import you.ru.model.Worker;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WorkerRepository extends JpaRepository<Worker, Long> {
-}
